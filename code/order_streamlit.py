bd2e0402d3058fa13b5b5f3c0e8373415d04fbdb
gyc36745 <trkvas811351@gmail.com>
2025-06-15 06:12:29 +0800

    Update order_streamlit.py

--- code/order_streamlit.py
+++ code/order_streamlit.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 # 下單部位管理物件
 class Record():
-    def __init__(self, G_spread=None, G_tax=None, G_commission=None):   ## 建構子
+    def __init__(self, G_spread=None, G_tax=None, G_commission=None,isFuture=None):   ## 建構子
         # 交易相關參數
         self.G_spread = G_spread
         self.G_tax = G_tax
