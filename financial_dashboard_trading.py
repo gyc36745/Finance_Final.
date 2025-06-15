cd26aa5411cf92bb77395d0ae506a3bb937e3c6c
gyc36745 <trkvas811351@gmail.com>
2025-06-15 14:21:31 +0800

    Update financial_dashboard_trading.py

--- financial_dashboard_trading.py
+++ financial_dashboard_trading.py
@@ -1034,6 +1034,7 @@ OrderRecord.GeneratorProfit_rateChart(StrategyName='MA')
 st.subheader("策略參數最佳化")
 
 import itertools
+from itertools import product
 
 short_range = range(3, 10)
 long_range = range(10, 30, 5)
