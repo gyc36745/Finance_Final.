1768b373fa40438233a3ac94b15bb551d4ac81e0
gyc36745 <trkvas811351@gmail.com>
2025-06-15 18:37:34 +0800

    Update financial_dashboard_trading.py

--- financial_dashboard_trading.py
+++ financial_dashboard_trading.py
@@ -1129,7 +1129,7 @@ if choice_strategy == choices_strategies[0]:
 	
 	
 	# -- 參數輸入 --
-	st.markdown("<h5>策略參數最佳化</h5>", unsafe_allow_html=True)
+	st.markdown("<h5>選取要試的參數範圍</h5>", unsafe_allow_html=True)
 	short_range = st.slider("短均線範圍", 1, 100, (5, 10))
 	long_range = st.slider("長均線範圍", 1, 100, (20, 30))
 	sl_value = st.slider("移動停損點數", min_value=1, max_value=100, value=30)
